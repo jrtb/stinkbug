6daed403e5441f16c5b698e3c98f7ae3d701e84e
James R T Bossert <jrbosser@gmail.com>
2013-11-12 15:11:57 -0500

    new cocos2d project, design files
    Signed-off-by: James R T Bossert <jrbosser@gmail.com>

---
+++ stinkbug/stinkbug/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  stinkbug
+//
+//  Created by jrtb on 11/12/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ stinkbug/stinkbug/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  stinkbug
+//
+//  Created by jrtb on 11/12/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
